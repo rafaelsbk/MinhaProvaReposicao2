5476edc2c0d96c70c605c53021926ae5b28e83bc
rafael.lima <rafael-ownz@hotmail.com>
2022-02-24 04:57:45 -0300

    PERMISSAO + TELAS - SEM TRANSIÇÃO + VIEW MODEL

--- app/src/main/java/com/example/minhaprovareposicao/MainActivity.kt
+++ app/src/main/java/com/example/minhaprovareposicao/MainActivity.kt
@@ -9,12 +9,10 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.findNavController
 import com.example.minhaprovareposicao.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: MainViewModel
-    lateinit var binding: FragmentHomeBinding
+
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     private var readPermissionOk = false
     private var writePermissionOk = false
@@ -22,11 +20,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModel = ViewModelProvider (this).get(MainViewModel::class.java)
-        binding.viewmodel = viewModel
-        binding.lifecycleOwner = this
-
 
         ///// PERMISSAO PRA ACESSAR O STORAGE EXTERNO
         permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
